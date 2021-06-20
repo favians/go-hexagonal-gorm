d4de03228881ef5745d896aaaa5791ab90417235
favians <faviansyah@alterra.id>
2021-06-20 08:22:03 +0700

    [2021/06/20]-Create Unit Test

---
+++ business/user/service_test.go
@@ -0,0 +1,132 @@
+package user_test
+
+import (
+	"go-hexagonal/business"
+	"go-hexagonal/business/user"
+	userMock "go-hexagonal/business/user/mocks"
+
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+const (
+	id       = "id"
+	name     = "name"
+	username = "username"
+	password = "password"
+	creator  = "creator"
+
+	modifier = "modifier"
+	version  = 1
+)
+
+var (
+	userService    user.Service
+	userRepository userMock.Repository
+
+	userData       user.User
+	insertUserData user.InsertUserSpec
+)
+
+func TestMain(m *testing.M) {
+	setup()
+	os.Exit(m.Run())
+}
+
+func TestFindUserByID(t *testing.T) {
+	t.Run("Expect found the user", func(t *testing.T) {
+		userRepository.On("FindUserByID", mock.AnythingOfType("string")).Return(&userData, nil).Once()
+
+		user, err := userService.FindUserByID(id)
+
+		assert.Nil(t, err)
+
+		assert.NotNil(t, user)
+
+		assert.Equal(t, id, user.ID)
+		assert.Equal(t, name, user.Name)
+		assert.Equal(t, username, user.Username)
+		assert.Equal(t, password, user.Password)
+
+	})
+
+	t.Run("Expect user not found", func(t *testing.T) {
+		userRepository.On("FindUserByID", mock.AnythingOfType("string")).Return(nil, business.ErrNotFound).Once()
+
+		user, err := userService.FindUserByID(id)
+
+		assert.NotNil(t, err)
+
+		assert.Nil(t, user)
+
+		assert.Equal(t, err, business.ErrNotFound)
+	})
+}
+
+func TestInsertUserByID(t *testing.T) {
+	t.Run("Expect insert user success", func(t *testing.T) {
+		userRepository.On("InsertUser", mock.AnythingOfType("user.User"), mock.AnythingOfType("string")).Return(nil).Once()
+
+		err := userService.InsertUser(insertUserData, creator)
+
+		assert.Nil(t, err)
+
+	})
+
+	t.Run("Expect insert user not found", func(t *testing.T) {
+		userRepository.On("InsertUser", mock.AnythingOfType("user.User"), mock.AnythingOfType("string")).Return(business.ErrInternalServerError).Once()
+
+		err := userService.InsertUser(insertUserData, creator)
+
+		assert.NotNil(t, err)
+
+		assert.Equal(t, err, business.ErrInternalServerError)
+	})
+}
+
+func TestUpdateUserByID(t *testing.T) {
+	t.Run("Expect update user success", func(t *testing.T) {
+		userRepository.On("FindUserByID", mock.AnythingOfType("string")).Return(&userData, nil).Once()
+		userRepository.On("UpdateUser", mock.AnythingOfType("user.User"), mock.AnythingOfType("int")).Return(nil).Once()
+
+		err := userService.UpdateUser(id, name, modifier, version)
+
+		assert.Nil(t, err)
+
+	})
+
+	t.Run("Expect update user failed", func(t *testing.T) {
+		userRepository.On("FindUserByID", mock.AnythingOfType("string")).Return(&userData, nil).Once()
+		userRepository.On("UpdateUser", mock.AnythingOfType("user.User"), mock.AnythingOfType("int")).Return(business.ErrInternalServerError).Once()
+
+		err := userService.UpdateUser(id, name, modifier, version)
+
+		assert.NotNil(t, err)
+
+		assert.Equal(t, err, business.ErrInternalServerError)
+	})
+}
+
+func setup() {
+
+	userData = user.NewUser(
+		id,
+		name,
+		username,
+		password,
+		creator,
+		time.Now(),
+	)
+
+	insertUserData = user.InsertUserSpec{
+		Name:     name,
+		Username: username,
+		Password: password,
+	}
+
+	userService = user.NewService(&userRepository)
+}
--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.16
 
 require (
 	github.com/go-playground/validator/v10 v10.6.1
-	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/echo/v4 v4.3.0 // indirect
+	github.com/labstack/echo/v4 v4.3.0
 	github.com/labstack/gommon v0.3.0
 	github.com/spf13/viper v1.8.0
+	github.com/stretchr/testify v1.7.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.3
 )
